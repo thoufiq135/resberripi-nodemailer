4723f06ffaa1094fdab23fd0c1945cb95a6be08a
Thoufiq <shaikno150@gmail.com>
2025-10-23 17:57:28 +0530

    e

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const token='2ce5d744098035fd03b9d552aab95444'
 const twilio=require('twilio')
 const client=twilio(sid,token)
 const port = 3001;
+let lat=0
+let log=0
+let sat=0
+let speed=0
 
 const app = express();
 app.use(cors());
@@ -27,16 +31,16 @@ app.post("/mail", async (req, res) => {
     let transport = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: "shaikno150@gmail.com",
-        pass: "ubvb xmif edli lrwa", 
+        user: "alimilidurgacharan@gmail.com",
+        pass: "usxi rswa qmih lalg", 
       },
     });
 
     await transport.sendMail({
-      from: "shaikno150@gmail.com",
+      from: "alimilidurgacharan@gmail.com",
       to: "cyrildavid1234@gmail.com",
       subject: "🚨 resberry pi ALERT!",
-      text: `${alert || 'Alert from ESP32'}\nHeart Rate: ${heart_rate || 'N/A'}\nSpO2: ${spo2 || 'N/A'}`|| "Alert from ESP32", 
+      text: `${alert || 'Alert from ESP32'}\nHeart Rate: ${heart_rate || 'N/A'}\nSpO2: ${spo2 || 'N/A'}\nlatitude:${lat||"no fix"}\nlogngitude:${log||"no fix"}\ncount:${sat||"no fix"}\nspeed:${speed||"no fix"}`|| "Alert from ESP32", 
     });
 
     console.log("Mail sent with cause:", alert);
@@ -57,23 +61,32 @@ app.post("/mail", async (req, res) => {
     res.status(500).json({ message: "Error sending mail", error: err.message });
   }
 });
-app.post("/heart",async(req,res)=>{
-  const {heart}=req.body;
-  console.log("heart=",heart)
-  let transport=nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-      user:"shaikno150@gmail.com",
-      pass:"ubvb xmif edli lrwa",
-    }
+app.post("/gps",async(req,res)=>{
+  const {latitude,logngitude,sati,speeds}=req.body;
+  console.log("lat=",latitude)
+  console.log("log=",logngitude)
+  console.log("sat=",sati)
+  console.log("speed=",speeds)
+if(latitude&&logngitude&&sati&&speeds){
+  lat=latitude
+  log=logngitude
+  sat=sati
+speed=speeds
+}
+  // let transport=nodemailer.createTransport({
+  //   service:"gmail",
+  //   auth:{
+  //     user:"shaikno150@gmail.com",
+  //     pass:"ubvb xmif edli lrwa",
+  //   }
 
-  })
-  await transport.sendMail({
-    from:"shaikno150@gmail.com",
-    to:"cyrildavid1234@gmail.com",
-    subject: "🚨 resberry pi ALERT!",
-      text: `${heart || 'Alert from ESP32'}`, 
-  })
+  // })
+  // await transport.sendMail({
+  //   from:"shaikno150@gmail.com",
+  //   to:"cyrildavid1234@gmail.com",
+  //   subject: "🚨 resberry pi ALERT!",
+  //     text: `${heart || 'Alert from ESP32'}`, 
+  // })
 })
 
 app.listen(port, () => {
